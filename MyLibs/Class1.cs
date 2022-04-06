23c9af95fdb746fa8a6436c9d958c756056752eb
claudy1337 <45602451+claudy1337@users.noreply.github.com>
2022-04-06 13:59:26 +0300

    Добавьте файлы проекта.

---
+++ MyLibs/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyLibs
+{
+    public class ReadIO
+    {
+        
+        public ReadIO()
+        {
+            Console.Write("file name:");
+            string str = Console.ReadLine();
+            Console.Write($"{str}.txt:");
+            string write = Convert.ToString(Console.ReadLine());
+            string filePath = @"D:\" + str + ".txt";
+            string text = write;
+            FileStream fileStream = null;
+            if (!File.Exists(filePath))
+                fileStream = File.Create(filePath);
+            else
+                fileStream = File.Open(filePath, FileMode.Append);
+            StreamWriter output = new StreamWriter(fileStream);
+            output.Write(text);
+            output.Close();
+        }  
+    }
+    public class Shutdow
+    {
+        public Shutdow()
+        {
+            Console.WriteLine("выключение через...");
+            System.Diagnostics.Process.Start("Shutdown", "-s -t 10");
+        }
+    }
+    public class CloseApp
+    {
+        public CloseApp()
+        {
+            Console.WriteLine("закрытие через...");
+            Environment.Exit(5);
+        }
+    }
+
+}
---
+++ SystemTasks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibs;
+using System.IO;
+
+namespace SystemTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("напишите комманду:");
+            string answer = Convert.ToString(Console.ReadLine());
+            if (answer == "read")
+            {
+                ReadIO read = new ReadIO();
+            }
+            else if (answer == "shutdown")
+            {
+                Shutdow shutdow = new Shutdow();
+                
+            }
+            else if(answer == "close")
+            {
+                CloseApp closeApp = new CloseApp();
+            }
+            
+        }
+    }
+}
